db0f085a258c004a191492a08bb62a118d6d6479
PhilipAAustin <philip.austin@colorado.edu>
2026-03-31 14:42:12 -0600

    FInished X discretization

--- NACA_Airfoils.m
+++ NACA_Airfoils.m
@@ -2,3 +2,17 @@ clc;
 clear;
 close all;
 
+m = 0;
+p = 0;
+t = 0;
+c=10;
+n = 12;
+
+
+%% Determining x discretization
+
+theta = linspace(0,pi,n/2+1); %
+
+x = (c.*cos(theta)+ c )./2
+
+X = [x,flip(x(1:end-1))]
