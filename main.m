3b0226ab2bd8311cd8386ed07194b222834d3dac
PhilipAAustin <philip.austin@colorado.edu>
2026-04-01 09:17:28 -0600

    main summary

--- main.m
+++ main.m
@@ -1,3 +1,11 @@
+%% ASEN 3111 - Computational Assignment XX - Main
+% Provide a breif summary of the problem statement and code so that
+% you or someone else can later understand what you attempted to do
+% it doesn't have to be that long.
+%
+% Co-Authors: Philip Austin, Charles Bailey, Nico Galindo, Natsumi Kakuda
+% Date: {should include the date last revised}
+
 clc; 
 clear;
 close all;
