b973ed8f575bf6fbd8a16cd1c71319ddc9f374ad
Natsumi Kakuda <aska7164@colorado.edu>
2026-04-07 13:57:39 -0600

    Update main.m

--- main.m
+++ main.m
@@ -1,5 +1,5 @@
 %% ASEN 3111 - Computational Assignment XX - Main
-% Provide a breif summary of the problem statement and code so that
+% Provide a brief summary of the problem statement and code so that
 % you or someone else can later understand what you attempted to do
 % it doesn't have to be that long.
 %
@@ -10,3 +10,16 @@ clc;
 clear;
 close all;
 
+% Task 1
+
+m_21 = 0;
+p_21 = 0;
+t_21 = 21;
+c_21 = 5;
+N_21 = 25;
+
+[x_21, y_21] = NACA_Airfoils(m_21,p_21,t_21,c_21,N_21);
+
+figure(1);
+plot(x_21, y_21);
+axis equal;
